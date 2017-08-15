66afca5991dd2f51c8efd7ad3309ce8f1dcc8b00
Heather Conley <hconley6695@gmail.com>
2017-08-14 21:37:58 -0400

    sliding pizzas now showing again--changed syntax for proper use of getElementById

--- README.md
+++ README.md
@@ -54,13 +54,32 @@ The portfolio was built on Twitter's <a href="http://getbootstrap.com/">Bootstra
 * <a href="http://getbootstrap.com/css/">Bootstrap's CSS Classes</a>
 * <a href="http://getbootstrap.com/components/">Bootstrap's Components</a>
 
-###MY NOTES
+### MY NOTES
 
-Optimizations that I have done:
+### Getting Started
+
+Clone the project from github:
+
+  ```bash
+  $> git clone https://github.com/hconley6695/mobile-portfolio.git
+  ```
+
+Serve the site:
+  ```bash
+  $> python -m SimpleHTTPServer 8000
+  ```
+Point your browser to your local host:
+  ```bash
+  $> localhost:8000
+  ```
+
+
+#### Optimizations that I have done:
 * Minified css and javascript files using Gulp 
 * Add "transform" and "will-change" to the class "randomPizzaContainer" for browser to anticipate the changes
 * Changed all instances of querySelector and querySelectorAll to the more efficient getElementById and getElementByClassName depending on whether a class or id is needed.
 * Changed out document.body request in "for" loop in changePizzaSizes function.  This prevents the browser from having to rerender the page every time the loop is run.  Added a style "for" loop within the changePizzaSizxes function to set the width, preventing the browser from having to render the page in between setting the styles.
+* Changed out document.body request in "for" loop in updatePositions function to prevent browser from rerendering the page every time it loops.
 
 
 
--- views/js/main.js
+++ views/js/main.js
@@ -406,13 +406,13 @@ var resizePizzas = function(size) {
   function changeSliderLabel(size) {
     switch(size) {
       case "1":
-        document.getElementById("#pizzaSize").innerHTML = "Small";
+        document.getElementById("pizzaSize").innerHTML = "Small";
         return;
       case "2":
-        document.getElementById("#pizzaSize").innerHTML = "Medium";
+        document.getElementById("pizzaSize").innerHTML = "Medium";
         return;
       case "3":
-        document.getElementById("#pizzaSize").innerHTML = "Large";
+        document.getElementById("pizzaSize").innerHTML = "Large";
         return;
       default:
         console.log("bug in changeSliderLabel");
@@ -424,7 +424,7 @@ var resizePizzas = function(size) {
    // Returns the size difference to change a pizza element from one size to another. Called by changePizzaSlices(size).
   function determineDx (elem, size) {
     var oldWidth = elem.offsetWidth;
-    var windowWidth = document.getElementById("#randomPizzas").offsetWidth;
+    var windowWidth = document.getElementById("randomPizzas").offsetWidth;
     var oldSize = oldWidth / windowWidth;
 
     // Changes the slider value to a percent width
@@ -516,7 +516,7 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.getElementsByClassName('.mover');
+  var items = document.getElementsByClassName('mover');
 
   var scrollIt = document.body.scrollTop / 1250;
 
@@ -542,15 +542,17 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
+
   for (var i = 0; i < 200; i++) {
-    var elem = document.createElement('img');
-    elem.className = 'mover';
+    var elem = document.createElement("img"); 
+    elem.className = "mover";
     elem.src = "images/pizza.png";
     elem.style.height = "100px";
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.getElementById("#movingPizzas1").appendChild(elem);
+    document.getElementById("movingPizzas1").appendChild(elem);
+
   }
   updatePositions();
 });
